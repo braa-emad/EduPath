73bcc0ed1dc9129f58f4fe585c75c6927cd536fc
braa-emad <112783591+braa-emad@users.noreply.github.com>
2024-12-01 13:44:32 +0200

    Update users.router.js

--- users.router.js
+++ users.router.js
@@ -1,37 +1,25 @@
-const controller = require('./users.controllers')
-const express = require('express');
+const controller = require("./users.controllers");
+const express = require("express");
 const router = express.Router();
-const {verifyToken} = require('./users.controllers');
-const passport = require('passport');
+const { verifyToken } = require("./users.controllers");
+const passport = require("passport");
 
+router.route("/").get(controller.Getallusers);
 
-router.route('/')
-    .get(controller.Getallusers);
+router.route("/register").post(controller.Register);
 
-router.route('/register')
-    .post(controller.Register);
+router.route("/login").post(controller.Login);
 
+router.route("/profile").post(controller.Profile);
 
-router.route('/login')
-    .post(controller.Login);
+router.route("/loginwithtoken").post(controller.LoginWithToken);
 
-router.route('/profile')
-    .post(controller.Profile);
+router.route("/resetpassword").post(controller.userResetPassword);
 
-router.route('/loginwithtoken')
-    .post(controller.LoginWithToken);
+router.route("/forgotpassword").post(controller.userForgotPassword);
 
+router.route("/otp").post(controller.reqOTP);
 
-router.route('/resetpassword')
-    .post(controller.userResetPassword);
-    
-router.route('/forgotpassword')
-    .post(controller.userForgotPassword);
+router.route("/checktoken").post(controller.Checktoken);
 
-router.route('/otp')
-    .post(controller.reqOTP);
-
-router.route('/checktoken')
-    .post(controller.Checktoken);
-
-module.exports = router
+module.exports = router;
