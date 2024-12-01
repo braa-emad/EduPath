176f7bcfebbd325bbd9aa37b97c6aba480e8cfaa
braa-emad <112783591+braa-emad@users.noreply.github.com>
2024-12-01 13:43:55 +0200

    Update user.model.js

--- user.model.js
+++ user.model.js
@@ -1,31 +1,30 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
+const mongoose = require("mongoose");
+const validator = require("validator");
 const UserSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password:{
-        type:String,
-        required: false
-    },
-    googleId: {
-        type: String,
-        unique: true,
-        sparse: true
-    },
-    otp:{
-        type: String,
-    },
-    otpExpiration:{
-        type: Date 
-    }
-    
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: false,
+  },
+  googleId: {
+    type: String,
+    unique: true,
+    sparse: true,
+  },
+  otp: {
+    type: String,
+  },
+  otpExpiration: {
+    type: Date,
+  },
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
